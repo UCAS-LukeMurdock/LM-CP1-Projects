3f574360d8b2c80e718132771c2e74f889384838
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-05 10:44:18 -0600

    WhoAreYou? Project

---
+++ whoAreYou/main.py
@@ -0,0 +1,9 @@
+#Luke Murdock, Who are you?
+
+name = input("What is your name?: ")
+
+age = input("What is your age?: ")
+
+color = input("What is your favorite color?: ")
+
+print("Hello,", age, "year old", name, "who loves", color,)
