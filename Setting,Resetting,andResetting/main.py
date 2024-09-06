84c55e5135307338ab80b7d06d77b9465b5db1de
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-06 10:51:17 -0600

    started table project

---
+++ Setting,Resetting,andResetting/main.py
@@ -0,0 +1,12 @@
+#Luke Murdock, Setting, Resetting, and Resetting
+
+faculty = 32
+students = 100
+student = students -1
+faculty = faculty -3
+guests = students * 2
+guests = guests -5
+table = (faculty+students+guests)/12
+table = str(table)
+
+print(table)
