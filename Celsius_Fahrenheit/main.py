6f20598d3ed4f38c6c2568f63d74dda0fd7f88bf
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-06 10:31:30 -0600

    Fahrenheit and Celsius Project

---
+++ Celsius_Fahrenheit/main.py
@@ -0,0 +1,11 @@
+#Luke Murdock, Celsius to Fahrenheit
+
+celsius = input("What is your temperature?: ")
+
+celsius = int(celsius)
+
+fahrenheit = (celsius * 9/5)+32
+
+fahrenheit = str(fahrenheit)
+
+print("Your temperature in celsius is", celsius, "and in farhrenheit is", fahrenheit)
