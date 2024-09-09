6e6e35958f47dc1db93a531418d0f3e472c00bbf
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-09 10:51:31 -0600

    More Tables

--- Setting,Resetting,andResetting/main.py
+++ Setting,Resetting,andResetting/main.py
@@ -2,11 +2,12 @@
 
 faculty = 32
 students = 100
-student = students -1
-faculty = faculty -3
+student = students - 1
+faculty = faculty - 3
 guests = students * 2
-guests = guests -5
-table = (faculty+students+guests)/12
+guests = guests - 15
+board = 1
+table = (faculty+students+guests+board)/12
 table = str(table)
 
-print(table)
+print("You need to bring", table, "tables")
