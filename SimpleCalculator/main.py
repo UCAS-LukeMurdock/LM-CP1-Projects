5b16b7917b6ceb422d3cfb0c0e1f55bf07d69be9
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-11 10:51:17 -0600

    Started Calculator

---
+++ SimpleCalculator/main.py
@@ -0,0 +1,12 @@
+#Luke Murdock, Simple Calculator
+
+operation = input("Which operation do you want to do? +, -, *, /, **, ?/, or %?:")
+
+num1 = input("What is your first numebr?:")
+num2 = input("What is your second numebr?:")
+
+num = float(num)
+num = float(num)
+
+answer = num1 operation num2 
+print(answer)
