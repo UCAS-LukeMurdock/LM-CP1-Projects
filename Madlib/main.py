20faeed392714f1a4af5076054eec8ea6c763c8a
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-12 10:42:37 -0600

    Madlib

---
+++ Madlib/main.py
@@ -0,0 +1,9 @@
+#Luke Murdock, Madlib
+
+noun = input("What is your noun?: ")
+adjective = input("What is your adjective?: ")
+verb = input("What is your verb that ends in ing?: ")
+person = input("What is a person's name?: ")
+friend = input("What is his friend's name?: ")
+
+print(person + "'s friend is " + friend + ". They are both " + adjective + ". Today they are " + verb + " with a " + noun + ". " + person + " and " + friend + " are " + verb + " because they are " + adjective + ".")
