e105f9a1cc0a811d2cecd6f4b5570e65c7656f4e
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-16 10:50:50 -0600

    Start Fibonacci

--- FibonacciSequence/main.py
+++ FibonacciSequence/main.py
@@ -1,12 +1,31 @@
 #Luke Murdock, Fibonacci Sequence
 
+def sequence(y, x):
+    z = y + x
+    return z
+
 num1 = 0
 num2 = 1
 
-def Sequence(y, x)
-    z =y + x
-    return z
+print(num1, num2)
 
-Fib = 
+fib1 = num1
+fib2 = num2
 
-print(Fib)
+num1 = num2
+num2 = sequence(num1, num2)
+
+print(num1, num2)
+
+fib3 = num1
+fib4 = num2
+
+num1 = num2
+num2 = sequence(num1, num2)
+
+print(num1, num2)
+
+fib5 = num1
+fib6 = num2
+
+print(fib1, fib2, fib3, fib4, fib5, fib6,)
