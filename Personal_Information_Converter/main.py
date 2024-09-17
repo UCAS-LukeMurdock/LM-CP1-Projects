d633780282a59f8ab93595266901a54530188a5c
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-17 10:38:01 -0600

    Info Center Program

---
+++ Personal_Information_Converter/main.py
@@ -0,0 +1,14 @@
+#Luke Murdock, Personal Information Converter
+
+name = input("What is your name?:")
+age = input("What is your age?:")
+num = input("What is your favorite number?:")
+height = input("What is your height in meters?:")
+
+print("Your name is:", name, " Your age is:", age, " Your favorite number is:", num, " Your height is:", height)
+
+age = int(age)
+num = int(num)
+height = float(height)
+
+print("Your name is:", name, " Your age is:", age, " Your favorite number is:", num, " Your height is:", height)
