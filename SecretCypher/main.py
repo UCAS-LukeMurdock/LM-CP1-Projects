d5bd9958fb06cdce5854c1938f553f6bb000924d
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-24 10:50:25 -0600

    Secret Cypher again

--- SecretCypher/main.py
+++ SecretCypher/main.py
@@ -1,5 +1,11 @@
 # #Luke Murdock, Secret Cypher
 
+
+
+
+
+
+
 # usersWord = input("What word do you want cyphered?: ")
 
 # def cypher(first, k):
@@ -11,19 +17,17 @@
 
 
 
-# print(cypher(usersWord))
+# # print(cypher(usersWord))
 
+# userWord = input("What word do you want cyphered?: ")
+# userShift = input("How many times do you want it shifted?: ")
 
-def rotate_list(lst, k):
-  """Rotates a list 'lst' by 'k' positions to the right."""
-  k = k % len(lst)  # Handle rotations larger than the list length
-  if (k > len(lst)) :
-    return "sorry. key is greater than length of list"
+# def rotate_list(lst, k):
+#   #"""Rotates a list 'lst' by 'k' positions to the right."""
+#   k = k % len(lst)  # Handle rotations larger than the list length
+#   if (k > len(lst)) :
+#     return "sorry. key is greater than length of list"
   
-  return lst[-k:] + lst[:-k]
+#   return lst[-k:] + lst[:-k]
 
-my_list = [1, 2, 3, 4, 5]
-word = list("Hello")
-rotated_list = rotate_list(word, 6)
-
-print(rotate_list())
+# print(rotate_list(userWord, userShift))
