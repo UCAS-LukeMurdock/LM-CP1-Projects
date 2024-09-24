4cfc291f453db465037ea37e394a1b60243682e0
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-09-24 10:13:32 -0600

    Anagram

--- AnagramCreator/main.py
+++ AnagramCreator/main.py
@@ -1,2 +1,14 @@
 #Luke Murdock, Anagram Creator
 
+import random
+
+name = input("What is your name?: ")
+userNum = int(input("How many anagrams do you want?: "))
+
+num = 1
+while num <= userNum:
+  letters = list(name.lower())  
+  random.shuffle(letters)
+  anagram = "".join(letters).title()
+  print(anagram)
+  num += 1
