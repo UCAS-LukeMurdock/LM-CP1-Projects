c69de537d976e11ae9bfbcb487865387795f17bf
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-01 10:51:03 -0600

    Debug2

--- Debugging/main.py
+++ Debugging/main.py
@@ -2,6 +2,14 @@
 
 # The function is supposed to print the second element of the list, but it causes an IndexError. Find the bug.
 
-my_list = [1, 2]
+#my_list = [1, 2]
 
-print(my_list[1])
+#print(my_list[1])
+
+def add_to_list(item, my_list=[]):
+
+     my_list.append(item)
+
+     return my_list
+
+print(add_to_list("apple", ["banana, orange, grape"]))
