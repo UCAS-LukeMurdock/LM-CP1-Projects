7813ea1dd2d550a16f2ea3d3ed9f88200ab2c43d
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-02 10:09:15 -0600

    Finish ClassList

---
+++ ClassList/main.py
@@ -0,0 +1,12 @@
+#Luke Murdock, List of Classes
+
+classList = ["Chemistry", "Programming", "Geography", "Seminary", "Englsih", "Drawing"]
+
+print(classList)
+
+print(classList[0])
+print(classList[1])
+print(classList[2])
+print(classList[3])
+print(classList[4])
+print(classList[5])
