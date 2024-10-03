3b52f74f0cb8c2fd23b6d06498937c8d76137a26
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-03 10:27:44 -0600

    FormatTest2

--- WhatAreTheseNumbers/main.py
+++ WhatAreTheseNumbers/main.py
@@ -1,11 +1,11 @@
 #Luke Murdock, What are these numbers?
 
-num = int(input("What is your number?:\n"))
+num = float(input("What is your number?:\n"))
 
-print("Your number with commas is {:,}".format(num))
+print("Your number with commas is {:,}".format(int(num)))
 
 print("Your number with 4 decimals is {:.4f}".format(num))
 
-print("Your number as a percentage is {:%}".format(num))
+print("Your number as a percentage is {:.0%}".format(num))
 
 print("Your number as money is ${:.2f}".format(num))
