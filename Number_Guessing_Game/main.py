9bb6ef418f6bab47e8487de4b5f73abd3a0dd94d
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-09 10:09:53 -0600

    Number Guess

--- Number_Guessing_Game/main.py
+++ Number_Guessing_Game/main.py
@@ -1,10 +1,11 @@
 #Luke Murdock, Number Guessing Game
 
+#Generate random number 1-10
 import random
-
 num = random.randrange(1,11)
 userNum = 0
 
+#Let the user guess until they get it right, with the computer telling them the direction
 while userNum != num:
     userNum = int(input("Guess a number between 1 and 10: "))
     if userNum > num:
