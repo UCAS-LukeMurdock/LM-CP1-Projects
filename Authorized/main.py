72a618a5b36a5705bc2b653e4f1a138016658e92
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-15 10:38:02 -0600

    Authorized

--- Authorized/main.py
+++ Authorized/main.py
@@ -1,1 +1,12 @@
 #Luke Murdock, Authorized
+
+admin = "Luke"
+author = ["George", "John", "Kristi", "Matt", "Matthew", "Cat", "Kaylee", "Lori", "Cam"]
+name = input("What is your name?: ")
+
+if name in author:
+    print("Welcome! You are authorized")
+elif name == admin:
+    print("You are admin!")
+else:
+    print("You are not authorized")
