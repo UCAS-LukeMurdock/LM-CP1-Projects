00792dc1fa2585c37d6eb66dee83b50bf4762e34
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-21 10:50:51 -0600

    Fix Game

--- MultiplicationTable/main.py
+++ MultiplicationTable/main.py
@@ -1,6 +1,13 @@
 #Luke Murdock, Multiplication Table
 
-userNum = int(input("Number?:"))
+#userNum = int(input("Number?:"))
 
-for num in range(13):
-    print(num * userNum)
+#for num in range(13):
+    #print(num * userNum)
+
+#table = []
+#for tableNum in range(13):
+    #for tableNum2 in range(tableNum):
+        #tabley = tableNum * tableNum
+        #table.append(tabley)
+#print(table)
