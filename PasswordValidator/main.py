c5e2d97ea23941185568e0c779858135016e6202
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-23 10:19:46 -0600

    Password Validator

--- PasswordValidator/main.py
+++ PasswordValidator/main.py
@@ -1,9 +1,11 @@
 #Luke Murdock, Password Validator
+
 word = ""
 accepted = False
-hasnum = False
+hasNum = False
+hasSpec = False
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-specials = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+specials = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "?", "/", ",", "<", ".", ">", "'"]
 
 while accepted == False:
     word = input("Password?: ")
@@ -11,18 +13,20 @@ while accepted == False:
         print("Too Short")
         continue
 
-    
+    for number in numbers:
+        if number in word:
+            hasNum = True
+    if hasNum == False:
+        print("Needs at least 1 number")
+        continue
 
-    #if 
+    for special in specials:
+        if special in word:
+            hasSpec = True
+    if hasSpec == False:
+        print("Needs at least 1 special character")
+        continue 
 
     accepted = True
 else:
     print("Your password has been accepted!")
-
-
-    for number in numbers:
-        if number in word:
-            hasnum = True
-    if hasnum == False:
-        print("Needs at least 1 number")
-        continue
