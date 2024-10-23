a6714b613cf98bcca94a9717d76667e54de8eb18
Luke Murdock <luke.murdock@ucas-edu.net>
2024-10-22 21:16:37 -0600

    RockPaperTest

--- Rock,Paper,Scissors/main.py
+++ Rock,Paper,Scissors/main.py
@@ -1,10 +1,51 @@
 #Luke Murdock, Rock, Paper, Scissors
+import random as r
 score = 0
 
 while True:
-    exit = int(input("Exit(1) Continue(2)\n"))
-    if exit == 1:
+    exit = 0
+    uChoice = 0
+    cChoice = 0
+    uWin = False
+    cWin = False
+    try: 
+        exit = int(input("Play Game(1) Exit(2)\n"))
+    except:
+        print("Wrong Input")
+        continue
+    if exit == 2:
         print("Score:", score, "\nEnd of Game")
         break
-    elif exit == 2:
-        print("Ok, Keep Going")
+    
+    try: 
+        uChoice = int(input("Rock(1) Paper(2) Scissors(3)\n"))
+    except:
+        print("Wrong Input")
+        continue
+        
+    cChoice = r.randint(1, 3)
+    print(cChoice)
+
+    if uChoice == 1 and cChoice == 2:
+            cWin = True
+    elif uChoice == 1 and cChoice == 3:
+            uWin = True
+    elif uChoice == 2 and cChoice == 3:
+            cWin = True
+    elif uChoice == 2 and cChoice == 1:
+            uWin = True
+    elif uChoice == 3 and cChoice == 1:
+            cWin = True
+    elif uChoice == 3 and cChoice == 2:
+            uWin = True
+    if uWin == True:
+        score += 1
+        print("You won!")
+    elif cWin == True:
+        score -= 1
+        print("You lost!")
+    elif uChoice == cChoice:
+        print("Tie!")
+    else:
+        print("Incorrect Number")
+        
