07b9e94ea69cc59fedf99370cb4b2e9487339297
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-10-30 10:51:42 -0600

    Time2

--- TimeT/main.py
+++ TimeT/main.py
@@ -3,6 +3,17 @@
 """
 SET time as 0
 
-
+while time less than 12
+ASK user what time it is
+Add answer to time
+Tell time
 
 """
+
+time =0
+while time <= 540:
+    time_pass = int(input("How many minutes has passed? "))
+    time += time_pass
+    hour = time/60
+    print(hour, "hours have passed")
+print("Its past your bedtime!")
