cde30dc5ac85894d0a15f810b0c01fc2c264bbfc
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-11-04 10:51:30 -0700

    Start of TicTacToe

--- TicTacToeGame/main.py
+++ TicTacToeGame/main.py
@@ -1,11 +1,20 @@
 #Luke Murdock, Tic-Tac-Toe-Game
 
+import random
 end = False
+
 board = [
     [1, 2 ,3],
     [4, 5 ,6],
     [7, 8 ,9],
 ]
 
+def check():
+    
+
 while end == False:
+    for list in board:
+        print(list)
+    choiceU = input("Where do you want to place X?")
+    if choiceU
     
