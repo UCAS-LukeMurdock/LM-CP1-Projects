1ac5ad6412ddfa461ab9a48244c89570b2ecc283
Luke Murdock <luke.murdock@ucas-edu.net>
2024-11-04 22:47:17 -0700

    CharacterCreate

--- CharacterCreator/main.py
+++ CharacterCreator/main.py
@@ -1,2 +1,57 @@
 #Luke Murdock, Character Creator
+health = 5
+strength = 5
+dexterity = 5
+intel = 5
 
+name = input("What is your character's name?\n")
+class_ = int(input("What is your character's class?\n Ranger(1) Scholar(2) Spy(3) Warrior(4)\n"))
+race = int(input("What is your character's class?\n Human(1) Elf(2) Orc(3) Dragon(4)\n"))
+
+if class_ == 1:
+    health += -1
+    strength += 0
+    dexterity += 2
+    intel += 1
+elif class_ == 2:
+    health += 0
+    strength += -1
+    dexterity += 1
+    intel += 2
+elif class_ == 3:
+    health += 2
+    strength += -1
+    dexterity += 1
+    intel += 0
+elif class_ == 4:
+    health += 1
+    strength += 2
+    dexterity += 0
+    intel += -1
+else:
+    print("Wrong Number")
+
+if race == 1:
+    health += 0
+    strength += 0
+    dexterity += 0
+    intel += 0
+elif race == 2:
+    health += 0
+    strength += -2
+    dexterity += 1
+    intel += 1
+elif race == 3:
+    health += 1
+    strength += 2
+    dexterity += 0
+    intel += -3
+elif race == 4:
+    health += 1
+    strength += 1
+    dexterity += -1
+    intel += -1
+else:
+    print("Wrong Number")
+
+print(f"{name} has a health of {health}, strength of {strength}, dexterity of {dexterity}, and intelligence of {intel}.")
--- TicTacToeGame/main.py
+++ TicTacToeGame/main.py
@@ -2,19 +2,49 @@
 
 import random
 end = False
+correctSpot = False
+winU = False
+winC = False
+tie = False
 
 board = [
     [1, 2 ,3],
     [4, 5 ,6],
     [7, 8 ,9],
-]
+    ]
 
 def check():
-    
+    global winU
+    global winC
+    global tie
 
+    for row in board:
+        if row[1] == ["X", "X", "X"]:
+            winU = True
+            end = True
+            return end
+
+
+input("Welcome to Tic-Tac-Toe! In this game you will play against a computer by deciding which space(number) to place your X's.\n")
 while end == False:
-    for list in board:
-        print(list)
-    choiceU = input("Where do you want to place X?")
-    if choiceU
-    
+    for row in board:
+        print(row)
+    choiceU = input("Where do you want to place X?\n")
+    for rowIndex, row in enumerate(board):
+        for spaceIndex, space in enumerate(row):
+            if str(space) == choiceU:
+                board[rowIndex][spaceIndex] = "X"
+    if check() == True:
+        continue
+
+    for row in board:
+        print(row)
+    while correctSpot == False:
+        choiceC = random.randrange(0,10)
+        for rowIndex, row in enumerate(board):
+            for spaceIndex, space in enumerate(row):
+                if str(space) == choiceC:
+                    board[rowIndex][spaceIndex] = "O"
+                    correctSpot = True
+    if check() == True:
+        continue
