e917692db1c8344055feab570dc3474953afc50b
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-11-19 10:13:48 -0700

    Histogram2

--- SimpleHistogram/main.py
+++ SimpleHistogram/main.py
@@ -7,9 +7,10 @@ num4 = int(input("Number 4:"))
 num5 = int(input("Number 5:"))
 num6 = int(input("Number 6:"))
 numlist = [num1, num2, num3, num4, num5, num6]
+numsList = [1, 2, 3, 4, 5, 6]
 
-for num in numlist:
+for index, num in enumerate(numlist):
     numA = ""
     for i in range(num):
         numA += "*"
-    print(f"{num}: {numA}")
+    print(f"{index + 1}: {numA}")
