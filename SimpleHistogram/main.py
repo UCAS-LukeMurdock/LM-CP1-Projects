1aa50cd44456d15a502f2f9d190d325389045f45
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-11-19 10:05:51 -0700

    Histogram

--- SimpleHistogram/main.py
+++ SimpleHistogram/main.py
@@ -1,13 +1,15 @@
 #Luke Murdock, Simple Histogram
 
-num1 = input("Number 1:")
-num2 = input("Number 2:")
-num3 = input("Number 4:")
-num4 = input("Number 5:")
-num5 = input("Number 6:")
-num6 = input("Number 17:")
+num1 = int(input("Number 1:"))
+num2 = int(input("Number 2:"))
+num3 = int(input("Number 3:"))
+num4 = int(input("Number 4:"))
+num5 = int(input("Number 5:"))
+num6 = int(input("Number 6:"))
 numlist = [num1, num2, num3, num4, num5, num6]
-while num[num]
-print(f"""
 
-""")
+for num in numlist:
+    numA = ""
+    for i in range(num):
+        numA += "*"
+    print(f"{num}: {numA}")
