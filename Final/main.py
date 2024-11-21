f3b82cf10a14bc72a364bf2bac029aba6c698d66
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-11-21 10:23:33 -0700

    start

---
+++ Final/main.py
@@ -0,0 +1,24 @@
+#Luke Murdock, Final Project
+
+def beach():
+
+def plains():
+
+def desert():
+
+def tundra():
+
+def forest():
+
+def mountain():
+
+def cliff():
+
+def canyon():
+
+def volcano():
+
+def flower():
+
+def oceanEnd():
+
