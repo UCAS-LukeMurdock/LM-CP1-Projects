cb568f5c5c39a32ac1b48c2476beb2a71c3735aa
UCAS-LukeMurdock <luke.murdock@ucas-edu.net>
2024-12-04 10:42:31 -0700

    MultiTable

--- Full Multiplication Table/main.py
+++ Full Multiplication Table/main.py
@@ -1,8 +1,9 @@
 #Luke Murdock, Full Multiplication Table
 
-row = ""
+
 for rownum in range(1, 13):
+    row = "|"
     for colnum in range(1, 13):
         num = str(rownum * colnum)
-        row += 
+        row += f" {num} |"
     print(row)
